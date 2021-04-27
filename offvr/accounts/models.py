d8e7422749a629b22ba605ed9a44f2d3144a0c37
milagyoshkova <gyoshkova.mila@gmail.com>
2021-04-27 16:44:06 +0300

    midol 5

--- offvr/accounts/models.py
+++ offvr/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name=maodels.CharField(max_longth=200,null=True)
+    phone=maodels.CharField(max_longth=200,null=True)
+    email=maodels.CharField(max_longth=200,null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null= True)
+    
