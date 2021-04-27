86e89b52715afcce1810ecb90e5ad174f08409cd
marti323 <marty323@abv.bg>
2021-04-27 21:37:05 +0100

    finalised 5th video

--- offvr/accounts/admin.py
+++ offvr/accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
- from . models import Customer
- admin .site.register(Customer)
+
+from . models import Customer
+
+admin .site.register(Customer)
--- offvr/accounts/models.py
+++ offvr/accounts/models.py
@@ -4,8 +4,33 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name=maodels.CharField(max_longth=200,null=True)
-    phone=maodels.CharField(max_longth=200,null=True)
-    email=maodels.CharField(max_longth=200,null=True)
+    name=models.CharField(max_length=200,null=True)
+    phone=models.CharField(max_length=200,null=True)
+    email=models.CharField(max_length=200,null=True)
     date_created=models.DateTimeField(auto_now_add=True,null= True)
     
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    data_created = models.DateTimeField(auto_now_add=True, null= True)
+
+class Order(models.Model):
+    STATUS = (
+        ('Fending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Deliered','Deliered'),
+    )
+
+    #customer = 
+    #product = 
+    data_created = models.DateTimeField(auto_now_add=True, null= True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
