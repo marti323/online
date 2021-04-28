226df83fdd7faea12785bc840a7d6523d8f85058
milagyoshkova <gyoshkova.mila@gmail.com>
2021-04-28 14:37:21 +0300

    Resolve Conflict

--- offvr/accounts/admin.py
+++ offvr/accounts/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-<<<<<<< HEAD
+
 from . models import *
+from . models import Customer
 
 admin .site.register(Customer)
 admin .site.register(Product)
 admin .site.register(Tag)
 admin .site.register(Order)
-=======
 
-from . models import Customer
 
-admin .site.register(Customer)
->>>>>>> 86e89b52715afcce1810ecb90e5ad174f08409cd
