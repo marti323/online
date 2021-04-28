a73be305282f226a55191dc22da4b469d182ed6b
milagyoshkova <gyoshkova.mila@gmail.com>
2021-04-28 13:26:11 +0300

    video 6

--- offvr/accounts/admin.py
+++ offvr/accounts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
- from . models import Customer
- admin .site.register(Customer)
+from . models import *
+
+admin .site.register(Customer)
+admin .site.register(Product)
+admin .site.register(Tag)
+admin .site.register(Order)
